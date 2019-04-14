20fbf1178f005c5ba9179f47c48f58459be169d7
mzukas <48844581+mzukas@users.noreply.github.com>
2019-04-15 01:21:15 +0300

    Add files via upload
    Papildyta pasirinkimu ar žinomas klasės namų darbų kiekis

--- 3 lab. 0.1.cpp
+++ 3 lab. 0.1.cpp
@@ -12,6 +12,7 @@ int main()
 {
     int n;
     int s = 0;
+    double v = 0;
     double V[256];
     double Vid[256];
     int kiekis;
@@ -20,6 +21,8 @@ int main()
     int paz[256];
     string Vardas[256];
     string Sarasas[256];
+
+
     cout << "Iveskite studentu kieki: ";
     cin >> n;
     while (!cin.good())
@@ -36,6 +39,12 @@ int main()
         getline(cin, Vardas[i-1]);
         s++;
     }
+
+char p;
+cout << "Ar zinote namu darbu kieki? t/n" << endl;
+cin >> p;
+if (p == 't') {
+
     for (int j = 0; j < s; j++)
     {
 
@@ -105,8 +114,82 @@ int main()
         cout << left << setw(35) << Vardas[o] << Vid[o] << endl;
     }
     }
+}
+else if (p == 'n')
+{
+
+   for (int j = 1; j <= s; j++)
+    {
+       int kiek[256];
+       int k = 0;
+       double vv[256];
+    {
+
+        int i = 1;
+        vv[j-1] = 0;
+        kiek[j-1] = 0;
+        char p;
+        cout << "Iveskite " << j << " mokinio " << i << " pazymi: ";
+        cin >> paz[i-1];
+        cout << "Ar visi pazymiai? t/n" << endl;
+        cin >> p;
+        vv[j-1] = vv[j-1] + paz[i-1];
+        while (p != 't') {
+
+            i++;
+            cout << "Iveskite " << j << " mokinio " << i << " pazymi: ";
+            cin >> paz[i-1];
+            vv[j-1] = vv[j-1] + paz[i-1];
+            cout << "Ar visi pazymiai? t/n" << endl;
+            cin >> p;
+
+        }
+        kiek[j-1] = kiek[j-1] + i;
+
+    }
+        k = k + kiek[j-1];
+        sort(paz, paz+k);
+        if (k%2 == 1)
+        med[j-1] = paz[(k/2)];
+        else if (((paz[k/2] + paz[(k/2) - 1])/2) % 2 != 0)
+            med[j-1] = (paz[k/2] + paz[(k/2) - 1])/2;
+        else if (((paz[k/2] + paz[(k/2) - 1])/2) % 2 == 0)
+            med[j-1] = (paz[k/2] + paz[(k/2) - 1])/2+0.5;
 
 
+        Vid[j-1] = vv[j-1] / kiek[j-1];
+        cout << k << endl;
+        cout << paz[1] << endl;
+        cout << "Vidurkis: " << setprecision(2) << fixed << Vid[j-1] << endl;
+        cout << "Mediana: " << setprecision(2) << fixed << med[j-1] << endl;
+
+
+    }
+
+
+
+
+    cout << "Rodyti mediana ar aritmetini vidurki? m/v" << endl;
+    cin >> p;
+
+    if (p == 'm') {
+    cout << string(79, '*') << endl;
+    cout << left << setw(30) << "Mokinys" << "Galutinis (Med.)" << endl;
+    for (int o = 0; o < s; o++)
+    {
+        cout << left << setw(35) << Vardas[o] << med[o] << endl;
+    }
+    }
+    if (p == 'v')
+    {
+    cout << string(79, '*') << endl;
+    cout << left << setw(30) << "Mokinys" << "Galutinis (Vid.)" << endl;
+    for (int o = 0; o < s; o++)
+    {
+        cout << left << setw(35) << Vardas[o] << Vid[o] << endl;
+    }
+    }
+}
     return 0;
 }
 
